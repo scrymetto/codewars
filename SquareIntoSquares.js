0b2461894c10e397281881ca14f621fbbac39e8f
hempy <hempy.scrymetto@gmail.com>
2019-02-25 14:49:47 +0300

    decompose index repeat

--- SquareIntoSquares.js
+++ SquareIntoSquares.js
@@ -4,7 +4,7 @@ function decompose(n) {
         let arrNew = [];
         for (let i = 0; i < arr.length; i++) {
             if (arrNew.indexOf(arr[i]) > 0) {
-                return false
+                return true
             } else {
                 arrNew.push(arr[i])
             }
@@ -23,11 +23,32 @@ function decompose(n) {
         }
     } while (summed);
 
-    if (isRepeat(result) === false) {
-        return false
-    } else {
-        return result
+    if (isRepeat(result) === true) {
+        let nextInd = result.length;
+        while (isRepeat(result) === true && nextInd > 0) {
+            for (let i = 1; i < result.length; i++) {
+                if (result[i] === result[i - 1]) {
+                    nextInd = i - 2;
+                    break;
+                }
+            }
+            let nextVal = result[nextInd];
+            console.log(nextVal);
+            result.splice(nextInd);
+            result.push(nextVal-1);
+            let sum = result.reduce((previousValue, currentValue) => previousValue+currentValue*currentValue);
+            console.log(sum);
+            residue = n*n-sum;
+            console.log(residue);
+            summed = result[result.length-1];
+            console.log(summed);
+        }
     }
+    //if (isRepeat(result) === true) {
+    //    return []
+    //} else {
+        return result
+    //}
 }
 
-console.log(decompose(5));
+console.log(decompose(50));
