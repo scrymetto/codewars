a84bf921c639ff754f3858dc94c87565ec2a0d7a
hempy <hempy.scrymetto@gmail.com>
2019-02-26 13:57:56 +0300

    decompose done!

--- SquareIntoSquares.js
+++ SquareIntoSquares.js
@@ -3,7 +3,7 @@ function decompose(n) {
     function isRepeat(arr) {
         let arrNew = [];
         for (let i = 0; i < arr.length; i++) {
-            if (arrNew.indexOf(arr[i]) > 0) {
+            if (arrNew.indexOf(arr[i]) >= 0) {
                 return true
             } else {
                 arrNew.push(arr[i])
@@ -11,44 +11,54 @@ function decompose(n) {
         }
     }
 
+    function findSquare(arr, summed, residue) {
+        do {
+            if (summed * summed > residue) {
+                summed--;
+            } else {
+                residue -= summed * summed;
+                result.push(summed)
+            }
+        } while (summed);
+        return result
+    }
+
     let result = [];
     let summed = n - 1;
     let residue = n * n;
-    do {
-        if (summed * summed > residue) {
-            summed--;
-        } else {
-            residue -= summed * summed;
-            result.push(summed)
-        }
-    } while (summed);
+
+    result = findSquare(result, summed, residue);
 
     if (isRepeat(result) === true) {
         let nextInd = result.length;
-        while (isRepeat(result) === true && nextInd > 0) {
+        while (isRepeat(result) === true) {
             for (let i = 1; i < result.length; i++) {
                 if (result[i] === result[i - 1]) {
                     nextInd = i - 2;
                     break;
                 }
             }
-            let nextVal = result[nextInd];
-            console.log(nextVal);
-            result.splice(nextInd);
-            result.push(nextVal-1);
-            let sum = result.reduce((previousValue, currentValue) => previousValue+currentValue*currentValue);
-            console.log(sum);
-            residue = n*n-sum;
-            console.log(residue);
-            summed = result[result.length-1];
-            console.log(summed);
+            if (nextInd < 0) {
+                nextInd = 0;
+            } else {
+                let nextVal = result[nextInd];
+                result.splice(nextInd);
+                result.push(nextVal - 1);
+                let sum = result.map((currentValue) => currentValue * currentValue).reduce(((previousValue, currentValue) => previousValue + currentValue));
+                residue = n * n - sum;
+                summed = result[result.length - 1];
+                result = findSquare(result, summed, residue)
+            }
+            if (isRepeat(result) === true && result[0] === 1) {
+                result = []
+            }
         }
     }
-    //if (isRepeat(result) === true) {
-    //    return []
-    //} else {
-        return result
-    //}
+    if (!result[0]) {
+        return null
+    } else {
+        return result.reverse()
+    }
 }
 
 console.log(decompose(50));
