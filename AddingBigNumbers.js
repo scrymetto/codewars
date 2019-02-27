1ea139ee3e034e377bd9049fbd32b09429840463
hempy <hempy.scrymetto@gmail.com>
2019-02-27 12:40:13 +0300

    Adding Big Numbers done

--- AddingBigNumbers.js
+++ AddingBigNumbers.js
@@ -1,5 +1,36 @@
 function add(a, b) {
-    return (Number(a) + Number(b)).toString(); // Fix me!
+
+    let result = [];
+    let summed = [];
+    let addend = [];
+    if (+a > +b) {
+        summed = a.split('').reverse();
+        addend = b.split('').reverse();
+    } else {
+        summed = b.split('').reverse();
+        addend = a.split('').reverse();
+    }
+
+    summed.forEach((value, index) => {
+        if (!addend[index]) {
+            addend[index] = 0
+        }
+        result.push(+value+ +addend[index])
+    });
+
+    let current = 0;
+    result = result.map((value, index) => {
+        let digit;
+        if (index!==result.length-1) {
+            digit = (current + value) % 10;
+            current = Math.floor((current + value) / 10)
+        } else {
+            digit = value+current
+        }
+        return digit
+    });
+
+    return result.reverse().join('')
 }
 
-console.log(add('63829983432984289347293874', '90938498237058927340892374089'));
+console.log(add('111', '9'));
