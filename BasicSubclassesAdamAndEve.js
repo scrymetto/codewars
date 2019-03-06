037b6884db86c551f4f062b551b75f62aba38de7
hempy <hempy.scrymetto@gmail.com>
2019-03-06 13:33:07 +0300

    Basic subclasses done

--- BasicSubclassesAdamAndEve.js
+++ BasicSubclassesAdamAndEve.js
@@ -1,11 +1,37 @@
-class God{
+class God {
 
-    human: 'man'
-
+    constructor () {}
     static create(){
-        return this.human
+        let adam = new Man('Adam');
+        let eve = new Woman('Eve');
+        let humans = [];
+        humans.push(adam, eve);
+        return humans
+    }
+}
+
+class Human  {
+
+    constructor (name) {
+        this.name = name
+    }
+}
+
+class Man extends Human {
+
+    constructor (name) {
+        super(name);
+    }
+}
+
+class Woman extends Human {
+
+    constructor (name) {
+        super(name);
     }
 }
 
 let humans = God.create();
-console.log(humans)
+
+console.log(humans);
+console.log(humans[0] instanceof Woman);
