7b5b29f359d073a2aa2215bc34cbf61c0cd8967f
hempy <hempy.scrymetto@gmail.com>
2019-03-19 13:41:09 +0300

    PaginationHelper done

--- PaginationHelper.js
+++ PaginationHelper.js
@@ -13,28 +13,25 @@ class PaginationHelper {
     }
 
     pageItemCount(pageIndex) {
-        let pageCount = this.pageCount();
-        let allItem = (pageIndex + 1) * this.itemsPerPage;
-        if (allItem > this.collection.length) {
+        let lastPage = this.pageCount() - 1;
+        let lastPageAmount = this.itemsPerPage - (this.pageCount() * this.itemsPerPage - this.itemCount());
+        if (pageIndex > lastPage) {
             return -1
+        } else if (pageIndex === lastPage) {
+            return lastPageAmount
         } else {
-
+            return this.itemsPerPage
         }
+    }
+
+    pageIndex(itemIndex) {
+        if (itemIndex < 0 || itemIndex >= this.itemCount()) {
+            return -1
+        }
+        return Math.floor(itemIndex / this.itemsPerPage);
     }
 }
 
 
-/*// returns the number of items on the current page. page_index is zero based.
-// this method should return -1 for pageIndex values that are out of range
-PaginationHelper.prototype.pageItemCount = function (pageIndex) {
-
-}
-
-// determines what page an item is on. Zero based indexes
-// this method should return -1 for itemIndex values that are out of range
-PaginationHelper.prototype.pageIndex = function (itemIndex) {
-
-}*/
-
-let helper = new PaginationHelper(['a', 'b', 'c', 'd', 'e', 'f'], 4);
-console.log(helper.pageItemCount(0)); //should == 6
+let helper = new PaginationHelper(['a', 'b', 'c', 'd', 'e', 'f', 'e'], 4);
+console.log(helper.pageIndex(0));
