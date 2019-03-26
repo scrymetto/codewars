3cb243e36d8177911a17fcce41d5998b533f0d6b
hempy <hempy.scrymetto@gmail.com>
2019-03-26 13:06:05 +0300

    Next smaller number with the same digits done but end-test

--- NextSmallerNumberWithTheSameDigits.js
+++ NextSmallerNumberWithTheSameDigits.js
@@ -1,27 +1,53 @@
 function nextSmaller(n) {
-    let arr = (n + '').split('');
-    let resultArr = [];
-    let result = -1;
-    for (let i = arr.length - 1; i >= 0; i--) {
-        let current = arr[i];
-        for (let j = arr.length - 1; j >= 0; j--) {
-            let tempArr = arr.slice();
-            let prev = arr[j];
-            if (prev > current) {
-                tempArr[j] = current;
-                tempArr[i] = prev;
-                if ((0 + tempArr.join('')) < n && tempArr[0] !== '0') {
-                    result = + tempArr.join('');
-                    resultArr.push(result);
+
+    let curArr = [-1];
+
+    function firstStep(n, m) {
+        if (!m) {
+            m = n
+        }
+        let arr = (n + '').split('');
+        let resultArr = [];
+        let result = -1;
+        for (let i = arr.length - 1; i >= 0; i--) {
+            let current = arr[i];
+            for (let j = arr.length - 1; j >= 0; j--) {
+                let tempArr = arr.slice();
+                let prev = arr[j];
+                if (prev > current) {
+                    tempArr[j] = current;
+                    tempArr[i] = prev;
+                    if ((0 + tempArr.join('')) < m &&tempArr[0] !== '0') {
+                        result = +tempArr.join('');
+                        if (resultArr.indexOf(result) < 0) {
+                            resultArr.push(result)
+                        }
+                    }
                 }
             }
         }
+        if (resultArr[0]) {
+            console.log('first '+ resultArr)
+            let curResult = resultArr.reduce((previousValue, currentValue) =>
+                previousValue > currentValue ? previousValue : currentValue);
+            console.log('twice  '+ curResult)
+
+            if (curArr.indexOf(curResult) < 0 && curResult>curArr[curArr.length-1]) {
+                curArr.push(curResult)
+            }
+            firstStep(curResult, n)
+        } else return -1
     }
-    if (resultArr[0]){
-        console.log(resultArr)
-        return resultArr.reduce((previousValue, currentValue) =>
-            previousValue>currentValue ? previousValue:currentValue);
+
+    firstStep(n);
+    if (curArr[0]) {
+        console.log(curArr)
+        return curArr.reduce((previousValue, currentValue) =>
+            previousValue > currentValue ? previousValue : currentValue)
     } else return -1
 }
 
-console.log(nextSmaller(123456908));
+console.log(nextSmaller(8455 ));
+
+
+
