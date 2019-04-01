cc4d25ae550dffae814a7668148681e31c5b4b16
hempy <hempy.scrymetto@gmail.com>
2019-04-01 13:26:01 +0300

    InstantRunoffVoting tie for least test

--- InstantRunoffVoting.js
+++ InstantRunoffVoting.js
@@ -11,47 +11,54 @@ function runoff(voters) {
             let winner1 = arr[i][0];
             result[winner1] ? result[winner1]++ : result[winner1] = 1;
         }
+        console.log(result)
         return result
     }
 
-    let result = calcVote(curVoters);
-    console.log(result);
-
-    let winner;
-    let max = 0;
-    let looser;
-    let min = 0;
-    for (let key in result) {
-        if (result[key] > max) {
-            winner = key;
-            max = result[key]
-        } else {
-            if (result[key] <= min) {
-                looser = key;
-                min = result[key]
+    function findWinner(obj) {
+        let winner;
+        let max = 0;
+        let looser = [];
+        let min = curVoters.length / 2;
+        for (let key in obj) {
+            let sign = obj[key];
+            if (sign > max) {
+                winner = key;
+                max = sign
+            } else if (sign <= min) {
+                let arr = looser[sign];
+                if (!looser[sign]){
+                    looser[sign] = [];
+                }
+                looser[sign].push(key);
+                min = sign
             }
         }
-    }
+        console.log(looser)
+        looser = looser.map(el =>{
 
-    if (max < (curVoters.length / 2)) {
-        curVoters = curVoters.map(arr => {
-            let ind = arr.indexOf(looser);
-            arr.splice(ind, 1);
-            return arr
         })
+
+        if (max < (curVoters.length / 2)) {
+            curVoters = curVoters.map(arr => {
+                let ind = arr.indexOf(looser);
+                arr.splice(ind, 1);
+                return arr
+            });
+            console.log(curVoters)
+            return [false, curVoters]
+        } else return [true, winner]
     }
-    console.log(curVoters);
 
-
-    result = calcVote(curVoters)
-    console.log(result);
-
-
-    return winner
+    let result = findWinner(calcVote(curVoters));
+    while (!result[0]) {
+        result = findWinner(calcVote(curVoters))
+    }
+    return result[1]
 }
 
-console.log(runoff([["c", "b", "a", "e", "d"],
-    ["e", "a", "c", "d", "b"],
-    ["c", "a", "b", "d", "e"],
-    ["d", "c", "a", "e", "b"],
-    ["a", "b", "c", "d", "e"]]));
+console.log(runoff([["a", "c", "d", "e", "b"],
+    ["e", "b", "d", "c", "a"],
+    ["d", "e", "c", "a", "b"],
+    ["c", "e", "d", "b", "a"],
+    ["b", "e", "a", "c", "d"]]));
