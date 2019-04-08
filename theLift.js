47bc76692c7fa66f495ed2a7e39f77173d3e1457
hempy <hempy.scrymetto@gmail.com>
2019-04-08 12:49:54 +0300

    theLift firstStep

--- theLift.js
+++ theLift.js
@@ -1,3 +1,22 @@
 let theLift = function (queues, capacity) {
-    return [999]
+    let task = {};
+    task.up = [];
+    task.down = [];
+    queues.forEach((val, ind)=>{
+        if (val.length>0){
+
+        }
+    })
+    return task
 };
+
+let queues = [
+    [], // G
+    [0, 3, 4], // 1
+    [], // 2
+    [], // 3
+    [2], // 4
+    [3], // 5
+    [], // 6
+];
+console.log(theLift(queues, 5))
