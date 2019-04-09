578c7c5fcbf2b5f1178277f4221016fa7b292090
hempy <hempy.scrymetto@gmail.com>
2019-04-09 13:51:35 +0300

    theLift function up - but not let out

--- theLift.js
+++ theLift.js
@@ -1,12 +1,53 @@
 let theLift = function (queues, capacity) {
+
+    let currentQueues = queues.concat();
     let task = {};
     task.up = [];
     task.down = [];
-    queues.forEach((val, ind)=>{
-        if (val.length>0){
+    let counter = 0;
+    let result = [];
+    result.push(0);
 
+    currentQueues.forEach((val, ind) => {
+        if (val.length > 0) {
+            task.down[ind] = [];
+            task.up[ind] = [];
+            val.forEach(to => {
+
+                if (to < ind) {
+                    task.down[ind].push(to);
+                }
+                if (to > ind) {
+                    task.up[ind].push(to);
+                }
+            })
         }
-    })
+    });
+
+    function up(task) {
+        let counter = 0;
+        task.forEach((val, ind) => {
+            if (counter === capacity){
+                return
+            }
+            if (val.length !== 0){
+                result.push(ind);
+                counter+=val.length;
+                val.forEach(to => {
+
+                })
+            }
+        })
+        return result
+    }
+
+    function down(queues, task) {
+
+    }
+
+    console.log(up(task.up))
+
+    result.push(0);
     return task
 };
 
@@ -16,7 +57,7 @@ let queues = [
     [], // 2
     [], // 3
     [2], // 4
-    [3], // 5
-    [], // 6
+    [6], // 5
+    [0], // 6
 ];
 console.log(theLift(queues, 5))
