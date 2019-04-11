4c07afd6b17ddc664137029ca616300fe4ff02b7
hempy <hempy.scrymetto@gmail.com>
2019-04-11 12:40:56 +0300

    theLift function up - done, but test (capacity)

--- theLift.js
+++ theLift.js
@@ -1,53 +1,83 @@
 let theLift = function (queues, capacity) {
 
     let currentQueues = queues.concat();
-    let task = {};
-    task.up = [];
-    task.down = [];
-    let counter = 0;
+    let allTask = {};
+    allTask.up = [];
+    allTask.down = [];
     let result = [];
-    result.push(0);
 
     currentQueues.forEach((val, ind) => {
+        allTask.down[ind] = [];
+        allTask.up[ind] = [];
         if (val.length > 0) {
-            task.down[ind] = [];
-            task.up[ind] = [];
             val.forEach(to => {
 
                 if (to < ind) {
-                    task.down[ind].push(to);
+                    allTask.down[ind].push(to);
                 }
                 if (to > ind) {
-                    task.up[ind].push(to);
+                    allTask.up[ind].push(to);
                 }
             })
         }
     });
-console.log(task)
-    function up(task) {
-        for (let i = 0; i<task.length; i++){
+    console.log(allTask)
 
+    function up(task) {
+        let curResult = [];
+        let count = 0;
+        let out = [];
+        let newTask = [];
+        for (let i = 0; i < task.length; i++) {
+            out.forEach(el => {
+                if (el === i) {
+                    curResult.push(i);
+                    count--;
+                }
+            });
+
+            if (task[i] && task[i].length > 0) {
+                curResult.push(i);
+                task[i].forEach(el => {
+                    if (count < capacity) {
+                        out.push(el);
+                        count++
+                    } else {
+                        if (newTask[i]) {
+                            newTask[i].push(el)
+                        } else {
+                            newTask[i] = [];
+                            newTask[i].push(el)
+                        }
+
+                    }
+                })
+            }
         }
+        allTask.up = newTask;
+        return Array.from(new Set(curResult))
     }
 
     function down(task) {
 
     }
 
-    let cur = up(task.up)
-    result = result.concat(cur)
+    let cur = up(allTask.up)
+    result = result.concat(cur);
+    if (result[0]!==0){
+        result.unshift(0)
+    }
     result.push(0);
-console.log(task)
     return result
 };
 
 let queues = [
     [], // G
-    [0, 4, 3], // 1
-    [3], // 2
-    [6], // 3
-    [2,5], // 4
-    [6], // 5
-    [0], // 6
+    [], // 1
+    [5, 5, 5], // 2
+    [], // 3
+    [], // 4
+    [], // 5
+    [], // 6
 ];
 console.log(theLift(queues, 5))
